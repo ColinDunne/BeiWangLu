13912473dd366496b848e220948080bb99fda9a9
QianChen <xgdjqc@126.com>
2015-03-09 14:58:41 +0800

    Initial commits

---
+++ BeiWangLu/BeiWangLu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BeiWangLu
+//
+//  Created by 钱辰 on 15/3/9.
+//  Copyright (c) 2015年 qianchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BeiWangLu/BeiWangLu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BeiWangLu
+//
+//  Created by 钱辰 on 15/3/9.
+//  Copyright (c) 2015年 qianchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
